9e090561bc3fadb7031d15c04556d7ee585a70b1
Elisa Tumwebaze <tumwebazeelisa@gmail.com>
2021-07-13 17:39:36 +0300

    changed the connection string

--- models/dbConfig.js
+++ models/dbConfig.js
@@ -3,12 +3,12 @@ let pg = require('pg');
 if(process.env.DATABASE_URL){
     pg.defaults.ssl = true;
 }
-const isProduction = process.env.NODE_ENV === 'production';
+c
 // Database connection 
 let connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.BD_PORT}/${process.env.DB_NAME}`
 const {Pool} = require('pg');
 
 const pool = new Pool({
-    connectionString:isProduction ? process.env.DATABASE_URL : connectionString
+    connectionString: process.env.DATABASE_URL
 });
 module.exports = pool;
