25f08e019b856482ce86b6b0619e976494217c6d
Elisa Tumwebaze <tumwebazeelisa@gmail.com>
2021-07-13 18:14:10 +0300

    edited the the database connection

--- models/dbConfig.js
+++ models/dbConfig.js
@@ -1,9 +1,17 @@
 const dotenv = require('dotenv').config();
 // Database connection 
-let connectionString = process.env.DATABASE_URL
 const {Pool} = require('pg');
+const poolConfig = process.env.DATABASE_URL ? {
+    connectionString : process.env.DATABASE_URL,
+    ssl:{rejectUnAuthorized : false}
+}:
+{
+    user : process.env.DB_USER,
+    password : process.env.DB_PASSWORD,
+    host : process.env.DB_HOST ,
+    port : process.env.DB_PORT,
+    database :process.env.DB_NAME
+};
 
-const pool = new Pool({
-    connectionString:connectionString 
-});
+const pool = new Pool(poolConfig);
 module.exports = pool;
