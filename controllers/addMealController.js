36b6f14300d5530ff8d1f2f2d3a6dd04185a61fa
Elisa Tumwebaze <tumwebazeelisa@gmail.com>
2021-09-08 15:03:39 +0300

    working cloudinary file uploa

--- controllers/addMealController.js
+++ controllers/addMealController.js
@@ -2,7 +2,6 @@ const pool = require('../models/dbConfig');
 const cloudinary = require('../utils/cloudinary');
 const validPrice = require('../utils/validatePrice');
 const validFoodName =require('../utils/validFoodName');
-const path = require('path');
 //add menu item controller
 module.exports = async(req,res)=>{
     const{foodname,price} = req.body
@@ -22,18 +21,19 @@ module.exports = async(req,res)=>{
     else if(!validPrice(price)){
         return res.status(400).json({error:'Invalid Price only numeric characters'});
     }
-    
-    try{
-        const result = await cloudinary.uploader.upload(req.file.path);
-        const imageUrl =result.secure_url;
-        const addFoodItem = await pool.query("INSERT INTO foods(food_name,price,photo) VALUES($1,$2,$3) RETURNING *",[foodname,price,imageUrl]);
-        if(addFoodItem){
-            const foodItem = await addFoodItem.rows[0];
-            res.status(201).json({message:foodItem})     
+    else{
+        try{
+            const result = await cloudinary.uploader.upload(req.file.path);
+            const imageUrl =result.secure_url;
+            const addFoodItem = await pool.query("INSERT INTO foods(food_name,price,photo) VALUES($1,$2,$3) RETURNING *",[foodname,price,imageUrl]);
+            if(addFoodItem){
+                const foodItem = await addFoodItem.rows[0];
+                res.status(201).json({message:foodItem})     
+            }
         }
-    }
-    catch(err){
-    console.error(err.message)
-    res.status(500).json({error:'server error'})
+        catch(err){
+        console.error(err.message)
+        res.status(500).json({error:'server error'})
+        }
     }
 }
