ef3f2de8c36a1780839a4a7e04e4b3cd2a07dd24
Elisa Tumwebaze <tumwebazeelisa@gmail.com>
2022-11-05 21:41:57 +0300

    deletede the delete photo functionality

--- controllers/deleteFoodController.js
+++ controllers/deleteFoodController.js
@@ -3,9 +3,6 @@ const { cloudinary } = require('../utils/cloudinary');
 module.exports = async(req,res)=>{
     const{id} = req.params;
     try{  
-        const photoID =await pool.query("SELECT cloudinary_id FROM foods WHERE food_id = $2",[id]);
-        const item =await photoID.rows[0].cloudinary_id; 
-        await cloudinary.uploader.destroy(item);
          
         const deleteFood = pool.query("DELETE FROM foods WHERE food_id = $1",[id]);
         if((await deleteFood)){
